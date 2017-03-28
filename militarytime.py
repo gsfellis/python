afcad9ecade5cd4153e9570f110dfa7114f3cd54
gsfellis <gsfellis@users.noreply.github.com>
2017-03-28 16:54:52 -0400

    Add files via upload

--- militarytime.py
+++ militarytime.py
@@ -1,18 +1,12 @@
 ## Converts military time, i.e. 0800, to standard time format, 12:00 AM
 
 def isMilitaryTime(time):
-    if time.isdigit() == False:        
-        return False
-
-    if len(time) != 4:        
-        return False
-
-    if int(time[:2]) > 24 or int(time[:2]) < 0:        
+    if (time.isdigit() == False
+            or len(time) != 4
+            or int(time[:2]) > 24
+            or int(time[2:4]) > 59):
         return False
         
-    if int(time[2:4]) > 59:
-        return False
-
     return True
 
 def convertMilToStandard(time):
@@ -28,8 +22,9 @@ def convertMilToStandard(time):
     if 12 < int(hours) < 24:
         period = 'PM'
         hours = str(int(hours) - 12)
-    elif int(hours) == 24:
+    elif int(hours) == 24 or int(hours) == 0:
         hours = '12'
+    
 
     return "{0}:{1} {2}".format(hours, minutes, period)
 
